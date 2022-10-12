958be2d460475f42ae728cd08800f4324e0a3c8a
Russell250 <89474081+Russell250@users.noreply.github.com>
2022-10-12 13:46:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ $dbname = "russtayl_sample";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+    echo $conn;
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
