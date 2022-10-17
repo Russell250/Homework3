fd7fa1f87d51b1ab5cf6bc6ecf753cc651b03026
Russell250 <89474081+Russell250@users.noreply.github.com>
2022-10-17 14:23:59 -0500

    Update actorfile.php

--- actorfile.php
+++ actorfile.php
@@ -1,1 +1,35 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <?php require_once("header.php"); ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Actors</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Actor's Movies</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "localhost";
+$username = "russtayl_user";
+$password = "RussTaylor2000";
+$dbname = "russtayl_sample";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+    
+$mid = $_GET["id"];
+$sql = "SELECT MovieName From Movie where ActorID =" . $mid;
+$result = 
 
