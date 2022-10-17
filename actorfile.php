ff8438f73b9ebede51da1f69e1c5e0967218d72d
Russell250 <89474081+Russell250@users.noreply.github.com>
2022-10-17 14:25:08 -0500

    Update actorfile.php

--- actorfile.php
+++ actorfile.php
@@ -33,3 +33,6 @@ $mid = $_GET["id"];
 $sql = "SELECT MovieName From Movie where ActorID =" . $mid;
 $result = 
 
+ if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
