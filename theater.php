fafd79427f6f77fb7f315e7f529b162f540dcee4
Russell250 <89474081+Russell250@users.noreply.github.com>
2022-10-17 14:41:26 -0500

    Update theater.php

--- theater.php
+++ theater.php
@@ -1,6 +1,4 @@
-<!doctype html>
-<html lang="en">
-  <head>
+
     <?php require_once("header.php"); ?>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,7 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Theath=ers</h1>
+    <h1>Theaters</h1>
 <table class="table table-striped">
   <thead>
     <tr>
@@ -30,7 +28,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * from Theater";
+$sql = "SELECT * from Theaters";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
