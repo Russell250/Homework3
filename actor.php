3535101388b998ae004f3bb38b157063278273bb
Russell250 <89474081+Russell250@users.noreply.github.com>
2022-10-19 14:08:49 -0500

    Update actor.php

--- actor.php
+++ actor.php
@@ -1,12 +1,6 @@
-<!doctype html>
-<html lang="en">
-  <head>
+
     <?php require_once("header.php"); ?>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Actors</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
+<title>Actors</title>
   <body>
     <h1>Actors</h1>
 <table class="table table-striped">
