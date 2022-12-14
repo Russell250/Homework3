ddb60a1a1bca113d048ce96929df5c41bb279f41
Russell250 <89474081+Russell250@users.noreply.github.com>
2022-12-13 18:58:34 -0600

    Update director.php

--- director.php
+++ director.php
@@ -33,16 +33,82 @@ if ($result->num_rows > 0) {
   <tr>
     <td><?=$row["DirectorID"]?></td>
     <td><?=$row["DirectorName"]?></td>
+    <td>
+         <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editActors<?=$row["DirectorID"]?>">
+                Edit
+              </button>
+              <div class="modal fade" id="editActors<?=$row["DirectorID"]?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editActors<?=$row["DirectorID"]?>Label" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="editActors<?=$row["DirectorName"]?>Label">Edit Director</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <form method="post" action="">
+                        <div class="mb-3">
+                          <label for="editActors<?=$row["DirectorID"]?>Name" class="form-label">Name</label>
+                          <input type="text" class="form-control" id="editActors<?=$row["DirectorID"]?>Name" aria-describedby="editActors<?=$row["DirectorID"]?>Help" name="iName" value="<?=$row['DirectorName']?>">
+                          <div id="editActors<?=$row["DirectorID"]?>Help" class="form-text">Enter the Director's name.</div>
+                        </div>
+                        <input type="hidden" name="iid" value="<?=$row['DirectorID']?>">
+                        <input type="hidden" name="saveType" value="Edit">
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td>
+                <form method="post" action="">
+                <input type="hidden" name="iid" value="<?=$row["DirectorID"]?>" />
+                <input type="hidden" name="saveType" value="Delete">
+                <input type="submit" class="btn" onclick="return confirm('Are you sure?')" value="Delete">
+              </form>
+    </td>
   </tr>
 <?php
-  }
-} else {
+        } 
+ } else {
   echo "0 results";
 }
-$conn->close();
 ?>
   </tbody>
     </table>
+<br />
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addActors">
+        Add New
+      </button>
+
+      <!-- Modal -->
+      <div class="modal fade" id="addActors" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addActorsLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="addActorsLabel">Add Item</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form method="post" action="">
+                <div class="mb-3">
+                  <label for="ActorName" class="form-label">Name</label>
+                  <input type="text" class="form-control" id="ActorName" aria-describedby="nameHelp" name="iName">
+                  <div id="nameHelp" class="form-text">Enter the Director's name.</div>
+                </div>
+                <input type="hidden" name="saveType" value="Add">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<?
+$conn->close();
+?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
