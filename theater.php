7de1b6e198abdf92e6805b9cfa27d565768803ce
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-12-14 19:48:54 -0600

    Update theater.php

--- theater.php
+++ theater.php
@@ -1,11 +1,5 @@
 
     <?php require_once("header.php"); ?>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Theater</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
-  <body>
     <h1>Theaters</h1>
 <table class="table table-striped">
   <thead>
@@ -79,7 +73,7 @@ if ($result->num_rows > 0) {
                     </div>
                     <div class="modal-body">
                         
-                      <form action="" method="post">
+                      <form method="post" action="">
                         <div class="mb-3">
                           <label for="editTheater<?=$row["TheaterID"]?>Name" class="form-label">Theater Name</label>
                           <input type="text" class="form-control" id="editTheater<?=$row["TheaterID"]?>Name" aria-describedby="editTheater<?=$row["TheaterID"]?>Help" name="tName" value="<?=$row['TheaterName']?>">
